79aa19be9c0bb0780ae9d1bef9ae29ccc71ad92f
Margarita (Rita) Busygina <busygina.rita.ca@gmail.com>
2025-03-26 17:15:03 -0400

    replace vanilla js with pug for server rendered templates

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import fs from "fs";
+
 const app = express();
 const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const jsonPath = path.join(__dirname, "data", "articles.json");
+const articles = JSON.parse(fs.readFileSync(jsonPath, "utf-8"));
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 app.use(express.static("public"));
 
 app.get("/(home)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.render("index", articles);
 });
 
-app.get("/home/:articleId", (req, res) => {
-  //   res.send(req.params);
-  res.sendFile(path.join(__dirname, "views", "article.html"));
+app.get("/article/:articleId", (req, res) => {
+  const articleId = parseInt(req.params.articleId);
+  res.render("article", {
+    articleId: articleId,
+    articles: articles.articles,
+  });
 });
 
-//serve static files
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
